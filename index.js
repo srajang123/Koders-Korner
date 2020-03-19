2a497c892af1e792ac86d6f973fff86a5ace740c
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2020-03-19 23:02:37 +0530

    Improved UI functioning and Javascript

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ a.addEventListener('click', e => {
     document.querySelector('.meminfo .mem-name').innerHTML = b.name;
     document.querySelector('.meminfo .mem-img').innerHTML = '<img src="src/img/srajan.jpg" height=100px width=60px>';
     c = b.profiles;
-    document.querySelector('.meminfo .mem-profiles').innerHTML = '<table class="profiles"> <tr><td><a href="' + c.codeforces + '">Codeforces</a></td><td><a href="' + c.codechef + '">Codechef</a></td><td><a href="' + c.hackerrank + '">Hackerrank</a></td><td><a href="' + c.github + '">Github</a></td><td><a href="' + c.stopstalk + '">StopStalk</a></td></tr></table>';
+    document.querySelector('.meminfo .mem-profiles').innerHTML = '<table class="profiles"> <tr><td><a href="https://www.codeforces.com/profile/' + c.codeforces + '" target="_blank">Codeforces</a></td><td><a href="https://www.codechef.com/users/' + c.codechef + '" target="_blank">Codechef</a></td><td><a href="https://www.hackerrank.com/' + c.hackerrank + '" target="_blank">Hackerrank</a></td><td><a href="https://www.github.com/' + c.github + '" target="_blank">Github</a></td><td><a href="https://www.stopstalk.com/user/profile/' + c.stopstalk + '" target="_blank">StopStalk</a></td></tr></table>';
     c = b.achievements.split(",");
     console.log(c);
     let w = '<h1 class="tit">Achievements</h1><ul class="ach">';
--- team.js
+++ team.js
@@ -4,7 +4,20 @@ close.addEventListener('click', e => {
 })
 let profile = document.querySelectorAll('.member');
 profile.forEach(el => {
-    el.addEventListener('click', e => {
-        document.querySelector('.meminfo').style.visibility = "Visible";
+        el.addEventListener('click', e => {
+            document.querySelector('.meminfo').style.visibility = "Visible";
+        })
     })
-})
+    //if(document.querySelector('.meminfo').style.visibility == "Visible" && )
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+        isEscape = (evt.key === "Escape" || evt.key === "Esc");
+    } else {
+        isEscape = (evt.keyCode === 27);
+    }
+    if (isEscape) {
+        document.querySelector('.meminfo').style.visibility = "Hidden"
+    }
+};
