7c0af45557821525712d82e69f415726c77eb7b1
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2020-03-27 11:18:28 +0530

    Added validation in Contact Us Page

---
+++ contact.js
@@ -0,0 +1,18 @@
+function CHK() {
+    if (document.querySelector('#name').value == "")
+        return Showerror("Please Enter Your Name");
+    else if (document.querySelector('.year').value == "-1")
+        return Showerror("Please select year");
+    else if (document.querySelector('.branch').value == "-1")
+        return Showerror("Please select Your Branch");
+    else if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.querySelector('#mail').value)))
+        return Showerror("Please enter a valid E-mail");
+    else if (document.querySelector("#query").value == "")
+        return Showerror("Please enter your Query");
+    else
+        return true;
+}
+let Showerror = (msg) => {
+    alert(msg);
+    return false;
+}
